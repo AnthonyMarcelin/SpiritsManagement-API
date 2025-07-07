a69a1aa70d10b8618871f1958d7757e0836aef0c
Anthony Marcelin <anthony.marcelin13@gmail.com>
2025-07-08 00:48:28 +0200

    add validation and schemas

--- src/routers/auth.router.js
+++ src/routers/auth.router.js
@@ -1,10 +1,12 @@
 import express from "express";
 import authController from "../controllers/auth.controller.js";
 import loginLimiter from "../middlewares/rateLimiter.middleware.js";
+import registerSchema from "../schemas/user.schema.js";
+import validateBody from "../middlewares/validateBody.middleware.js";
 
 const authRouter = express.Router();
 
-authRouter.post("/register", authController.register)
+authRouter.post("/register",validateBody(registerSchema), authController.register)
 authRouter.post("/login", loginLimiter, authController.login)
 
 export default authRouter;
